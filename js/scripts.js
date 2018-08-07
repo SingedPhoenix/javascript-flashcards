de3f204ae159180f272976225caf38b75027e561
Student <example@example.com>
2018-08-07 14:56:16 -0700

    adds css, js, html

---
+++ js/scripts.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+  $(".javascript").hover(function() {
+    $("#javascript").fadeToggle();
+  });
+
+  $(".operators").hover(function() {
+    $("#operators").fadeToggle();
+  });
+
+  $(".variables").hover(function() {
+    $("#variables").fadeToggle();
+  });
+
+  $(".varinameconv").hover(function() {
+    $("#varinameconv").fadeToggle();
+  });
+
+  $(".functions").hover(function() {
+    $("#functions").fadeToggle();
+  });
+
+  $(".methods").hover(function() {
+    $("#methods").fadeToggle();
+  });
+
+  $(".arguments").hover(function() {
+    $("#arguments").fadeToggle();
+  });
+
+  $(".parameters").hover(function() {
+    $("#parameters").fadeToggle();
+  });
+
+  $(".return").hover(function() {
+    $("#return").fadeToggle();
+  });
+
+  $(".changing-methods").hover(function() {
+    $("#changing-methods").fadeToggle();
+  });
+
+  $(".strings").hover(function() {
+    $("#strings").fadeToggle();
+  });
+
+  $(".booleans").hover(function() {
+    $("#booleans").fadeToggle();
+  });
+
+  $(".undefined").hover(function() {
+    $("#undefined").fadeToggle();
+  });
+
+  $(".walrus").hover(function() {
+    $("#walrus").fadeToggle();
+  });
+
+  $(".nan").hover(function() {
+    $("#nan").fadeToggle();
+  });
+
+  $(".escape").hover(function() {
+    $("#escape").fadeToggle();
+  });
+
+  $(".alerts").hover(function() {
+    $("#alerts").fadeToggle();
+  });
+
+  $(".comments").hover(function() {
+    $("#comments").fadeToggle();
+  });
+
+  $(".jquery").hover(function() {
+    $("#jquery").fadeToggle();
+  });
+
+  $(".attributes").hover(function() {
+    $("#attributes").fadeToggle();
+  });
+});
